b3f179b8dfb978c258ddbc37a8d7e35be15340bf
phuccoder <anhdeptraippt@gmail.com>
2025-07-03 22:16:42 +0700

    update gradle, banner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.android.gms:play-services-auth-api-phone:18.0.1")
     implementation("com.google.android.gms:play-services-safetynet:18.0.1")
+    implementation ("com.google.firebase:firebase-appcheck-debug:17.1.2")
+    implementation ("com.google.firebase:firebase-storage:20.3.0")
+    implementation ("com.firebaseui:firebase-ui-storage:8.0.2")
 
     // Network libraries
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
